1ea4eb93ed15b3f436ad8772f8d302430418c6e9
Subham Kumar Sharma <subhamrelax45@gmail.com>
2024-08-15 13:53:17 +0530

    minor

--- frontend/src/Component/Stock.jsx
+++ frontend/src/Component/Stock.jsx
@@ -1,44 +1,39 @@
-
 import axios from "axios";
-export const API_KEY = "R15KIHP60KJKIDS7"; // Alpha Vantage API key
+
+export const API_KEY = "R15KIHP60KJKIDS7"; // Replace with your API key
 
 const alphaClient = axios.create({
-    baseURL: 'https://www.alphavantage.co/query',
+  baseURL: 'https://www.alphavantage.co/query',
 });
 
 export const getLiveStockPrices = (symbol, callback) => {
-    alphaClient.get('', {
-        params: {
-            function: 'GLOBAL_QUOTE',
-            symbol,
-            apikey: API_KEY,
-        },
-    })
-    .then(response => {
-        console.log(response.data); // Log the data to the console
-        callback(null, response.data);
-    })
-    .catch(error => {
-        console.error('Error fetching stock price:', error); // Log the error
-        callback(error, null);
-    });
+  alphaClient.get('', {
+    params: {
+      function: 'GLOBAL_QUOTE',
+      symbol,
+      apikey: API_KEY,
+    },
+  })
+  .then(response => {
+    callback(null, response.data);
+  })
+  .catch(error => {
+    callback(error, null);
+  });
 };
 
 export const getData = (symbol) => {
-    return alphaClient.get('', {
-        params: {
-            function: 'TIME_SERIES_DAILY', // Correct function name
-            symbol: symbol,
-            apikey: API_KEY, // Correct API key parameter
-        },
-    })
-    .then(response => {
-        console.log(response.data); // Log the data to the console
-        // callback(null, response.data);
-    })
-    .catch(error => {
-        console.error('Error fetching stock price:', error); // Log the error
-        // callback(error, null);
-    });
-}
-console.log(getData('AAPL'));
+  return alphaClient.get('', {
+    params: {
+      function: 'TIME_SERIES_DAILY',
+      symbol,
+      apikey: API_KEY,
+    },
+  })
+  .then(response => {
+    return response.data;
+  })
+  .catch(error => {
+    throw error;
+  });
+};
